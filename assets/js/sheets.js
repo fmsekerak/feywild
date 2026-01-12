9c870f2d576df7cf127871aa89ff9f8ea8330622
Faith Sekerak <59906879+fmsekerak@users.noreply.github.com>
2026-01-12 14:39:04 -0500

    ?

--- assets/js/sheets.js
+++ assets/js/sheets.js
@@ -1,12 +1,13 @@
-const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGEGjryoMoYyFZIWPFrYLLO9M9Z0zq0lbIB4xIe-_-VqRwAQ6KP2ley9HpuDokO9i07lbDD4CnKqVT/pub?output=csv";
+const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGEGjryoMoYyFZIWPFrYLLO9M9Z0zq0lbIB4xIe-_-VqRwAQ6KP2ley9HpuDokO9i07lbDD4CnKqVT/pub?gid=0&single=true&output=csv";
 
 fetch(sheetURL)
-  .then(response => response.text())
+  .then(res => res.text())
   .then(csv => {
-    const rows = csv.split("\n").map(row => row.split(","));
-    console.log(rows);
+    const data = csvToObjects(csv);
+    console.log(data);
   });
 
+const list = document.getElementById("items");
 
 function csvToObjects(csv) {
   const lines = csv.trim().split("\n");
@@ -20,16 +21,6 @@ function csvToObjects(csv) {
     }, {});
   });
 }
-
-
-fetch(sheetURL)
-  .then(res => res.text())
-  .then(csv => {
-    const data = csvToObjects(csv);
-    console.log(data);
-  });
-
-const list = document.getElementById("items");
 
 data.forEach(item => {
   const li = document.createElement("li");
